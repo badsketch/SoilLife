a35869e1c0d0e500b2b8aeca3822492a30c2309a
DanielWang405 <Danielwang405@gmail.com>
2018-10-12 00:51:39 -0700

    init setup

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+})
+
+app.listen(3000);
+
+console.log('listening on port 3000...')
