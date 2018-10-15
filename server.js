b9eb31e2c335016c9604fff12282355bad6c9fb1
DanielWang405 <Danielwang405@gmail.com>
2018-10-14 23:38:46 -0700

    added secondary nav partial w/ variable that determins background color

--- server.js
+++ server.js
@@ -9,6 +9,26 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 })
 
+
+app.get('/food', function(req, res) {
+    res.render('pages/food');
+})
+app.get('/fiber', function(req, res) {
+    res.render('pages/fiber');
+})
+app.get('/filter', function(req, res) {
+    res.render('pages/filter');
+})
+app.get('/foundation', function(req, res) {
+    res.render('pages/foundation');
+})
+app.get('/farmaceuticals', function(req, res) {
+    res.render('pages/farmaceuticals');
+})
+app.get('/fun', function(req, res) {
+    res.render('pages/fun');
+})
+
 app.listen(3000);
 
 console.log('listening on port 3000...')
