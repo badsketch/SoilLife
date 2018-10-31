7b00f97a3efd8f25556a271c8485bbfae217eb39
DanielWang405 <Danielwang405@gmail.com>
2018-10-31 02:48:03 -0700

    forgot heroku port

--- server.js
+++ server.js
@@ -36,8 +36,9 @@ app.get('/about', function(req, res) {
 app.get('/media', function(req, res) {
     res.render('pages/media');
 })
-app.listen(port, function() {
-    console.log('Our app is running');
-});
 
-// console.log('listening on port 3000...')
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('our app is running');
+});
